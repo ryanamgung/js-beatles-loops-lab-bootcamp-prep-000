62b5ecd752c470981c43a2dab02a34f7b90c24d3
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 06:06:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function theBeatlesPlay(musicians, instruments)
 {
-  var stringArr = {};
+  var stringArr = [];
   for(let i = 0; i < musicians.length; i++)
   {
     var string = `$(musicians[i]) plays $(instruments[i])`
     stringArr.push(string)
   }
+  return stringArr
+}
+
+function johnLennonFacts(facts)
+{
+  var i = 0;
+  while(i < facts.length)
+  {
+    facts[i] += "!!!"
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number)
+{
+  var arr = [];
+  do{
+    arr.push("I love the Beatles!")
+    number++
+  }while(number < 15);
 }
