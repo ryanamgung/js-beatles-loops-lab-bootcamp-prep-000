cf091727f3e0f74265158bb1b03916db471abb44
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 06:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments)
   var stringArr = [];
   for(let i = 0; i < musicians.length; i++)
   {
-    stringArr.push(musicians[i] + "plays" + instruments[i])
+    stringArr.push(musicians[i] + " plays " + instruments[i])
   }
   return stringArr
 }
