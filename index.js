79df5554c8b363c369bbb428cf8211b89114e5ac
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 06:01:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments)
+{
+  var counter = {};
+  for(let i = 0; i < musicians.length; i++)
+  {
+    var string = `$(musicians[i]) plays $(instruments[i])`
+  }
+}
